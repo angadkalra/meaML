e7c1a24b21930ed2dca858117261bce407e34df3
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2018-02-03 18:04:32 -0800

    Loaded dataset and modified values.

--- recommendation.py
+++ recommendation.py
@@ -1,9 +1,16 @@
 import numpy as np
 import sklearn as skl
 
-# Load dataset
+# Load dataset: User ID, Movie ID, Rating (1/0)
+X = np.loadtxt('ratings_small.csv', delimiter=',', skiprows=1, usecols=(0,1,2))
+n,d = X.shape
 
 # Modify dataset to binary ratings
+for i in range(n):
+	if X[i,2] >= 3.0:
+		X[i,2] = 1
+	else:
+		X[i,2] = 0
 
 # Create sparse user vectors
 
