53e95264fdcde190528379eb0da2b341a7485901
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2018-02-03 17:36:18 -0800

    Building KNN script...

---
+++ recommendation.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sklearn as skl
+
+# Load dataset
+
+# Modify dataset to binary ratings
+
+# Create sparse user vectors
+
+# Given random binary vector, find KNN
+
+# Output user ID's
