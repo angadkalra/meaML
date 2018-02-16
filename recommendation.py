10dd11ee492e28fc7fd4543cbfb906a788164cf6
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2018-02-16 14:14:41 -0800

    Improving script.

--- recommendation.py
+++ recommendation.py
@@ -1,41 +1,43 @@
 import numpy as np
 import sklearn as skl
 import scipy as scp 
+import sys
 
-# Load dataset: User ID, Movie ID, Rating (1 or 0)
-X = np.loadtxt('ratings_small.csv', delimiter=',', skiprows=1, usecols=(0,1,2))
-n,d = X.shape
+# Load dataset: User ID, Movie ID, Rating
+X = np.loadtxt('ratings_small.csv', dtype=[('userID',np.int_),('movieID',np.int_),('rating',np.float_)], 
+               delimiter=',', skiprows=1, usecols=(0,1,2))
+
+n = X.shape[0] # number of ratings
+uIDs = 672 # number of user IDs + 1
 
 # Modify dataset to binary ratings
 for i in range(n):
-	if X[i,2] >= 3.0:
-		X[i,2] = 1
+	if X[i][2] >= 3.0:
+		X[i][2] = 1
 	else:
-		X[i,2] = 0
+		X[i][2] = 0
 
-# Create dictionary with user as key and array of tuples as value where each tuple 
-# is (movie_id, 1)
+# Create dictionary with user as key and array of movieIDs that were selected (value = 1)
 R = {}
 
 for i in range(n):
-    userID = X[i,0]
+    userID = X[i][0]
     
-    if X[i,2] == 0:
+    if X[i][2] == 0:
         continue
     
     if userID in R.keys():
-        R[userID].append(X[i,1])
+        R[userID].append(X[i][1])
     else:
-        R[userID] = [X[i,1]]
+        R[userID] = [X[i][1]]
 
 users = R.keys()
 
 # Given user id, find KNN (k = 6 for now)
 def knn(uid):
-# Go through every user, compute euclid. distance & store (dist, id) in ndarray. 
-# Sort array based on closest dist. Return 6 closest users. 
+    # Go through every user, compute euclid. distance & store (dist, id) in ndarray.  
     
-    distances = np.ndarray((n,2), dtype=(np.int_,np.int_))
+    distances = np.ndarray((uIDs,2), dtype=(np.int_,np.int_)) # Each row is (distance, userID)
     
     uid_movies = np.asarray(R[uid])
     
@@ -49,7 +51,8 @@ def knn(uid):
         dist = differences.size
         distances[u,0] = dist
         distances[u,1] = u
-        
     
-    return 0
+    # Sort array based on closest dist. Return 6 closest users.
+    np.savetxt('distances.txt', distances, fmt='%1d', delimiter=',')
     
+    return
